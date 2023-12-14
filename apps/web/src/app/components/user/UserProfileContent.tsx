e541d32842357c343a29ac3f0dbb8e91bd03cab3
Conner Chyung <connerchyung@gmail.com>
2023-12-14 15:12:34 -0500

    remove saved lists tab

--- apps/web/src/app/components/user/UserProfileContent.tsx
+++ apps/web/src/app/components/user/UserProfileContent.tsx
@@ -11,18 +11,18 @@ const UserProfileContent = ({ lists }: { lists: List[] }) => {
     <div className="w-full">
       <Tab.Group>
         <Tab.List className="w-full flex flex-row">
-          <Tab className="focus:outline-none py-2 w-1/3 flex flex-row items-center justify-center gap-1 text-gray border-b-2 border-b-gray ui-selected:text-primary ui-selected:border-b-primary">
+          <Tab className="focus:outline-none py-2 w-1/2 flex flex-row items-center justify-center gap-1 text-gray border-b-2 border-b-gray ui-selected:text-primary ui-selected:border-b-primary">
             <ListsIcon className="h-5" />
             <span className="caption-light">Lists</span>
           </Tab>
-          <Tab className="focus:outline-none py-2 w-1/3 flex flex-row items-center justify-center gap-1 text-gray border-b-2 border-b-gray ui-selected:text-primary ui-selected:border-b-primary">
+          <Tab className="focus:outline-none py-2 w-1/2 flex flex-row items-center justify-center gap-1 text-gray border-b-2 border-b-gray ui-selected:text-primary ui-selected:border-b-primary">
             <DraftsIcon className="h-5" />
             <span className="caption-light">Drafts</span>
           </Tab>
-          <Tab className="focus:outline-none py-2 w-1/3 flex flex-row items-center justify-center gap-1 text-gray border-b-2 border-b-gray ui-selected:text-primary ui-selected:border-b-primary">
+          {/* <Tab className="focus:outline-none py-2 w-1/3 flex flex-row items-center justify-center gap-1 text-gray border-b-2 border-b-gray ui-selected:text-primary ui-selected:border-b-primary">
             <SaveIcon className="h-5" />
             <span className="caption-light">Saved</span>
-          </Tab>
+          </Tab> */}
         </Tab.List>
         <Tab.Panels>
           <Tab.Panel className="mx-auto max-w-6xl p-3">
@@ -41,9 +41,9 @@ const UserProfileContent = ({ lists }: { lists: List[] }) => {
               )}
             />
           </Tab.Panel>
-          <Tab.Panel className="mx-auto max-w-6xl p-3">
+          {/* <Tab.Panel className="mx-auto max-w-6xl p-3">
             <p>Saved Lists (TODO)</p>
-          </Tab.Panel>
+          </Tab.Panel> */}
         </Tab.Panels>
       </Tab.Group>
     </div>
