499900153f4da30eb0049a9e127abebed12d8de0
Conner Chyung <connerchyung@gmail.com>
2023-12-27 16:10:42 -0500

    fix padding problem

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ApolloWrapper } from "@/lib/api/apolloProvider";
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { CreateListModalProvider } from "./components/list/create/CreateListModalProvider";
 import { UserProvider } from "@/app/components/user/UserProvider";
@@ -12,6 +12,16 @@ export const metadata: Metadata = {
   manifest: "/manifest.json", // we are accessing our manifest file here
   title: "Breezi",
   description: "Welcome to Breezi. Sign up today!",
+  appleWebApp: true,
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
@@ -22,11 +32,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg">
       <head>
-        <meta
-          name="viewport"
-          content="initial-scale=1, viewport-fit=cover, width=device-width"
-        ></meta>
-        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
