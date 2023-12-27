8e8c00a94e0ad40e59a7f6b28308024defd1cb03
Conner Chyung <connerchyung@gmail.com>
2023-12-27 15:56:50 -0500

    play around with pwa settings

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,7 +6,6 @@ import { CreateListModalProvider } from "./components/list/create/CreateListModa
 import { UserProvider } from "@/app/components/user/UserProvider";
 import AmplitudeProvider from "./lib/analytics/AmplitudeProvider";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,6 +21,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg">
+      <head>
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover, width=device-width"
+        ></meta>
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        ></meta>
+      </head>
       <UserProvider>
         <ApolloWrapper>
           <AmplitudeProvider>
